3a03253c0749fb01e63e19863dc4167c1d43f1bd
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-02-15 20:28:08 +0700

    try to show all data

--- mongodb.js
+++ mongodb.js
@@ -6,11 +6,20 @@ const uri = "mongodb://127.0.0.1:27017";
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
-client
-  .connect()
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Establish and verify connection
+    await client.db("db_test").command({ ping: 1 });
+    console.log("Connected successfully to server");
+
+    const database = client.db("db_test");
+    const res = await database.collection("users").find().toArray();
+    console.log(res);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
